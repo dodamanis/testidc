0d6b9e038301af8893e52beb97660e58ddf0f629
dodamanis <66838528+dodamanis@users.noreply.github.com>
2023-02-25 23:38:30 +0530

    Update TestidcApplication.java

--- src/main/java/com/testidc/testidc/TestidcApplication.java
+++ src/main/java/com/testidc/testidc/TestidcApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class TestidcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestidcApplication.class, args);
 	}
+	
+	@RequestMapping("/message")
+	public String message() {
+
+	return "App is running";
+}
 
 }
