d33486ecb7586155f0152af727e9580d837dadc5
dodamanis <66838528+dodamanis@users.noreply.github.com>
2023-02-25 23:37:58 +0530

    Update ServletInitializer.java

--- src/main/java/com/testidc/testidc/ServletInitializer.java
+++ src/main/java/com/testidc/testidc/ServletInitializer.java
@@ -6,19 +6,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TestidcApplication.class);
 	}
-
-	@RequestMapping("/message")
-public String message() {
-
-	return "App is running";
-}
 
 }
